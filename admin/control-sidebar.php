9202f4d25d224fcc0ca034e60c7cf7ec0cd15e4b
m1racdursun <dursunmirac2@gmail.com>
2024-12-27 04:50:50 +0300

    control-sidebar düzenlendi.

--- admin/control-sidebar.php
+++ admin/control-sidebar.php
@@ -1,7 +1,7 @@
  <!-- Control Sidebar -->
  <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
-    <div class="p-3">
+    <div class="p-1">
       <h5>Title</h5>
       <p>Sidebar content</p>
     </div>
